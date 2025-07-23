302c1359a28f4f89b8225c7e9938be3da1788501
Admin <mohameddione14@gmail.com>
2025-07-23 13:31:52 +0000

    integration du loader et de l'accueil

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,17 @@
 const Loader = () => {
     return (
         <div>
-            <h1>Rechercher</h1>
+            <section className="container">
+      <div className="row text-center">
+        <div className="col-md-12 col-sm-6">
+          <div className="d-flex align-items-center justify-content-center">
+            <i className="fa-solid fa-carrot fa-3x text-white"></i>
+            <h1 className="text-white ms-2"><a href="accueil.html" className="navbar-brand">nectar</a></h1>
+          </div>
+          <h5 className="ms-5 text-white">Online groceriet</h5>
+        </div>
+      </div>
+    </section>
         </div>
     )
 }
